9bb8403931b54f5360890ce7e0366460117a8b7c
Andreas JungÂ <info@zopyx.com>
2016-03-30 19:46:01 +0200

    updated

--- xmldirector/dropbox/browser/__init__.py
+++ xmldirector/dropbox/browser/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
 ################################################################
-# xmldirector.crex
+# xmldirector.dropbox
 # (C) 2015,  Andreas Jung, www.zopyx.com, Tuebingen, Germany
 ################################################################
