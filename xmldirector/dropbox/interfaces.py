5cd05ed4baa5de9d537d38502c57bae8fd6bd427
Andreas JungÂ <info@zopyx.com>
2016-03-30 19:32:57 +0200

    updated

--- xmldirector/dropbox/interfaces.py
+++ xmldirector/dropbox/interfaces.py
@@ -7,43 +7,22 @@
 
 from zope.interface import Interface
 from zope import schema
-from xmldirector.crex.i18n import MessageFactory as _
+from xmldirector.dropbox.i18n import MessageFactory as _
 
 
 class IBrowserLayer(Interface):
     """A brower layer specific to my product """
 
 
-class ICRexSettings(Interface):
-    """ C-Rex settings """
+class IDropboxSettings(Interface):
+    """ Dropbox settings """
 
-    crex_conversion_url = schema.TextLine(
-        title=_(u'URL for C-REX conversion webservice'),
-        description=_(u'URL for C-REX conversion webservice'),
-        default=u'https://c-rex.net/api/XBot/Convert/DGHO/docxMigration'
+    dropbox_app_key = schema.TextLine(
+        title=_(u'Dropbox application key'),
+        required=True
     )
 
-    crex_conversion_username = schema.TextLine(
-        title=_(u'Username for C-REX conversion webservice'),
-        description=_(u'Username for C-REX conversion webservice'),
-        default=u''
-    )
-
-    crex_conversion_password = schema.TextLine(
-        title=_(u'Password for C-REX conversion webservice'),
-        description=_(u'Password for C-REX conversion webservice'),
-        default=u''
-    )
-
-    crex_conversion_token = schema.TextLine(
-        title=_(u'C-REX Conversion Token'),
-        description=_(u'C-REX Conversion Token'),
-        default=u'',
-        required=False
-    )
-
-    crex_conversion_token_last_fetched = schema.Datetime(
-        title=_(u'DateTime C-Rex conversion toked fetched'),
-        description=_(u'DateTime C-Rex conversion toked fetched'),
-        required=False
+    dropbox_app_secret = schema.TextLine(
+        title=_(u'Dropbox application secret'),
+        required=True
     )
