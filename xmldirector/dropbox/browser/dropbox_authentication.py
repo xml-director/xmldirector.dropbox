a51131b2e75b1dc49de88e59e143c1b2f9893a54
Andreas JungÂ <info@zopyx.com>
2016-07-22 10:00:27 +0200

    fix

--- xmldirector/dropbox/browser/dropbox_authentication.py
+++ xmldirector/dropbox/browser/dropbox_authentication.py
@@ -26,7 +26,7 @@ DROPBOX_TEMP_TOKEN = 'xmldirector.dropbox.oauth_temporary_token'
 class DropboxAuthentication(BrowserView):
 
     def __init__(self, context, request):
-        alsoProvides(self.request, IDisableCSRFProtection) # fuck all Plone protection shit!
+        alsoProvides(request, IDisableCSRFProtection) # fuck all Plone protection shit!
         super(DropboxAuthentication, self).__init__(context, request)
 
     def authorize(self, oauth_token):
